ef7232fb6d5486894f07d6b125b2d54f566f3e6b
Kuntal Majumder <hellozee@disroot.org>
2019-12-15 19:37:31 +0530

    completed vector class

---
+++ yart/Vector.cs
@@ -0,0 +1,125 @@
+using System;
+
+namespace yart
+{
+    public class Vec3
+    {
+        private readonly double _x, _y, _z;
+        
+        public Vec3()
+        {
+            _x = _y = _z = 0;
+        }
+
+        public Vec3(double val)
+        {
+            _x = _y = _z = val;
+        }
+
+        public Vec3(double x, double y, double z)
+        {
+            _x = x;
+            _y = y;
+            _z = z;
+        }
+
+        public double Getx()
+        {
+            return _x;
+        }
+        
+        public double Gety()
+        {
+            return _y;
+        }
+
+        public double Getz()
+        {
+            return _z;
+        }
+        
+        public static Vec3 operator +(Vec3 v1, Vec3 v2)
+        {
+            return new Vec3(v1.Getx() + v2.Getx(),
+                v1.Gety() + v2.Gety(),
+                v1.Getz() + v2.Getz());
+        }
+
+        public static Vec3 operator -(Vec3 v1, Vec3 v2)
+        {
+            return new Vec3(v1.Getx() - v2.Getx(),
+                v1.Gety() - v2.Gety(),
+                v1.Getz() - v2.Gety());
+        }
+
+        public static Vec3 operator *(Vec3 v, double val)
+        {
+            return new Vec3(v.Getx() * val,
+                v.Gety() * val,
+                v.Getz() * val);
+        }
+
+        public static Vec3 operator *(double val, Vec3 v)
+        {
+            return v * val;
+        }
+
+        public static Vec3 operator *(Vec3 v1, Vec3 v2)
+        {
+            return new Vec3(v1.Getx() * v2.Getx(),
+                v1.Gety() * v2.Gety(),
+                v1.Getz() * v2.Getz());
+        }
+
+        public static Vec3 operator /(Vec3 v1, Vec3 v2)
+        {
+            return new Vec3(v1.Getx()/v2.Getx(),
+                v1.Gety()/v2.Gety(),
+                v1.Getz()/v2.Getz());
+        }
+
+        public static Vec3 operator /(Vec3 v, double val)
+        {
+            return new Vec3(v.Getx()/val,
+                v.Gety()/val,
+                v.Getz()/val);
+        }
+
+        public double Magnitude()
+        {
+            return Math.Sqrt(_x * _x + _y * _y + _z * _z);
+        }
+
+        public Vec3 Normalize()
+        {
+            return this/this.Magnitude();
+        }
+
+        public static double Dot(Vec3 v1, Vec3 v2)
+        {
+            return v1.Getx() * v2.Getx() + v1.Gety() * v2.Gety() + v1.Getz() * v2.Getz();
+        }
+
+        public static Vec3 Cross(Vec3 v1, Vec3 v2)
+        {
+            return new Vec3(v1.Gety() * v2.Getz() - v1.Getz() * v2.Gety(),
+                v1.Getz() * v2.Getx() - v1.Getx() * v2.Getz(),
+                v1.Getx() * v2.Gety() - v1.Gety() * v2.Getx());
+        }
+        
+        public static bool operator ==(Vec3 v1, Vec3 v2)
+        {
+            return v1.Getx() == v2.Getx() && v1.Gety() == v2.Gety() && v1.Getz() == v2.Getz();
+        }
+
+        public static bool operator !=(Vec3 v1, Vec3 v2)
+        {
+            return !(v1 == v2);
+        }
+
+        public override string ToString()
+        {
+            return _x + " " + _y + " " + _z;
+        }
+    }
+}
